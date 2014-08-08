a48d608f549705ea03e523b07328c8337043d6de
mrsmothers <mrsmothers@gmail.com>
2014-08-08 13:04:54 -0700

    Create bsWordPressPlugIn.php

---
+++ bsWordPressPlugIn.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Coming Next
+Plugin URI: http://nettuts.com
+Description: Shows the next scheduled posts with brief descriptions.
+Version: 1.0
+Author: Jarkko Laine
+Author URI: http://jarkkolaine.com
+*/
+?>

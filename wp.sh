53f24ab6326e38d49c6be3bca5deb9baa6e42eac
mrsmothers <mrsmothers@gmail.com>
2014-08-08 16:18:46 -0700

    Update wp.sh

--- wp.sh
+++ wp.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-echo “Database Name: ”
-read -e dbname
-echo “Database User: ”
-read -e dbuser
-echo “Database Password: ”
-read -s dbpass
-echo “run install? (y/n)”
-read -e run
-if [ "$run" == n ] ; then
-exit
-else
+BASE = "base"
+mkdir -p "$BASE/$1"
+
+cd "$BASE/$1"
+
 #download wordpress
 curl -O http://wordpress.org/latest.tar.gz
 #unzip wordpress
@@ -25,9 +19,9 @@ rm -R wordpress
 #create wp config
 cp wp-config-sample.php wp-config.php
 #set database details with perl find and replace
-sed -e “s/database_name_here/$dbname/g” wp-config.php
-sed -e “s/username_here/$dbuser/g” wp-config.php
-sed “s/password_here/$dbpass/g” wp-config.php
+sed -e "s/database_name_here/$1/g" wp-config.php
+sed -e "s/username_here/$2/g" wp-config.php
+sed "s/password_here/$3/g" wp-config.php
 #create uploads folder and set permissions
 mkdir wp-content/uploads
 chmod 777 wp-content/uploads
